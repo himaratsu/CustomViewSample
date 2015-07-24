9d783be5a50fdcdeefc39bcc1642e5be030523ad
Ryosuke Hiramatsu <himaratsu@gmail.com>
2015-07-24 19:51:14 +0900

    Initial Commit

---
+++ CustomViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomViewSample
+//
+//  Created by himara2 on 2015/07/24.
+//  Copyright (c) 2015年 himara2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
