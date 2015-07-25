f558501f8b72aa3c500b6b4d95bc932802eb2f6c
Ryosuke Hiramatsu <himaratsu@gmail.com>
2015-07-26 00:22:57 +0900

    codeから初期化する例を追加

--- CustomViewSample/ViewController.swift
+++ CustomViewSample/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // use from code
+//        let customView = MyCustomView(frame: CGRectMake(50, 100, 270, 200))
+//        customView.titleLabel.text = "Hello!"
+//        view.addSubview(customView)
     }
 }
 
