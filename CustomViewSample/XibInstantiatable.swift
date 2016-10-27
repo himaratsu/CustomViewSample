a5660d589a0878b813fdf508463dcd1a7441e239
Ryosuke Hiramatsu <rhiramat@yahoo-corp.jp>
2016-10-27 09:19:48 +0900

    Added: extension separating common method

---
+++ CustomViewSample/XibInstantiatable.swift
@@ -0,0 +1,30 @@
+//
+//  XibInstantiatable.swift
+//  CustomViewSample
+//
+//  Created by 平松　亮介 on 2016/10/27.
+//  Copyright © 2016年 himara2. All rights reserved.
+//
+
+import UIKit
+
+protocol XibInstantiatable {
+    func instantiate()
+}
+
+extension XibInstantiatable where Self: UIView {
+    func instantiate() {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: String(describing: type(of: self)), bundle: bundle)
+        guard let view = nib.instantiate(withOwner: self, options: nil).first as? UIView else {
+            return
+        }
+        addSubview(view)
+
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        let bindings = ["view": view]
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[view]|", options:NSLayoutFormatOptions(rawValue: 0), metrics:nil, views: bindings))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[view]|", options:NSLayoutFormatOptions(rawValue: 0), metrics:nil, views: bindings))
+    }
+}
